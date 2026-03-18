06bad02622f17f584f110925e6372ba693d7da40
vic-cavalcant3 <victorrocha0223@gmail.com>
2026-03-17 22:15:06 -0300

    exercicios ate o 6 feitooo

--- 4-L1-VICTORR.c
+++ 4-L1-VICTORR.c
@@ -31,9 +31,38 @@
  * ---------------------------------------------------------------------------
  */
 
+// Online C compiler to run C program online
 #include <stdio.h>
 
 int main() {
+    float num;
+    int leitura;
     
-    return 0;
+    do{
+        printf("Digite o  numero: ");
+        leitura = scanf("%f" , &num);
+        if(leitura != 1 ){
+             printf("Entrada invalida!! Tente novamente.\n");
+             scanf("%*s");
+        }
+    }while (leitura != 1);
+    
+    if(num > 0){
+        printf("O numero é positivo!!\n");
+    }else if (num < 0 ){
+        printf("O numero é negativo!!\n");
+    }else{
+        printf("O numero é zero!!");
+    }
+    
+    if(num == (int)num ){
+        printf("O numero é inteiro!!! \n");
+        if ((int) num % 2 == 0)
+        printf("O numero é par \n");
+        else
+            printf("O numero é impar\n");
+    }else{
+        printf("O numero é decimal!!");
+    }
+    
 }
--- 5-L1-VICTORR.c
+++ 5-L1-VICTORR.c
@@ -24,9 +24,27 @@
  * ---------------------------------------------------------------------------
  */
 
-#include <stdio.h>
+ #include <stdio.h>
 
-int main() {
-    
-    return 0;
-}
+ int main() {
+     float lado1, lado2, base;
+ 
+     printf("Digite a base: ");
+     scanf("%f", &base);
+     printf("Digite o lado1: ");
+     scanf("%f", &lado1);
+     printf("Digite o lado2: ");
+     scanf("%f", &lado2);
+ 
+     if (base <= 0 || lado1 <= 0 || lado2 <= 0) {
+         printf("Os lados tem que maiores que zero!!!\n");
+     } else if (base == lado1 && lado1 == lado2) {
+         printf("\nO triangulo e equilatero");
+     } else if (base == lado1 || base == lado2 || lado1 == lado2) {
+         printf("\nO triangulo e isosceles");
+     } else {
+         printf("O triangulo e escaleno\n");
+     }
+ 
+     return 0;
+ }
